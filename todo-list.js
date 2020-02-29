760f20473f77c749f4cf06bfded688292effae21
Charlie Bandstra <bandstracharlie@gmail.com>
2020-02-29 01:45:36 -0700

    removed unnecessary method

--- todo-list.js
+++ todo-list.js
@@ -15,6 +15,7 @@ class List {
   }
 
   saveToStorage(list, lists) {
+    debugger
     lists.push(list)
     var stringedLists = JSON.stringify(lists);
     localStorage.setItem('lists', stringedLists)
@@ -25,10 +26,6 @@ class List {
     if(parsedLists) {
       displayStoredLists(parsedLists);
     }
-  }
-
-  retrieveFromStorage() {
-
   }
 
   deleteFromStorage() {
